a159d4f7d75293370b5adb3d097851df26d4c4ec
AngeFlower <angefleurisseineza@gmail.com>
2021-08-24 14:52:48 +0200

    add cpmponents

---
+++ src/components/List_produit.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<table>
+			<thead>
+				<th>name</th>
+				<th>prix</th>
+				<th>date</th>
+				<th>categorie</th>
+				<th>marque</th>
+				<th>image_devant</th>
+				<th>image_arriere</th>
+				<th>image_face</th>
+			</thead>
+			<tbody>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tbody>
+		</table>
+	</div>
+	
+</template>
+
+<script type="">
+	export default{
+
+	}
+
+</script>
+
+<style type="">
+	
+
+</style>
